3f78005b58461e679cf98c824195dc9ffcaa914d
Maatangy <148937742+Maatangy@users.noreply.github.com>
2024-10-19 12:19:28 +0530

    Create odoreven.java

---
+++ odoreven.java
@@ -0,0 +1,11 @@
+``` java
+class Solution{
+    static String oddEven(int num) {
+        if (num % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+     }
+   }
+```
