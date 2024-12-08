fc2ab1692eb6ee301ad98336a1f2d99456ba3e52
Maatangy <148937742+Maatangy@users.noreply.github.com>
2024-12-08 10:06:42 +0530

    Create  two values x and y. Prints x for y number of times..java

---
+++ two values x and y. Prints x for y number of times..java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int X = scan.nextInt();
+        int Y = scan.nextInt();
+        for(int i=1 ; i<=Y ; i++){
+            System.out.println(X);
+        }
+    }
+}
