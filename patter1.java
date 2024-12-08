b6bd81d1cc52f8a3dad01cc4d5bf613df12f3efd
Maatangy <148937742+Maatangy@users.noreply.github.com>
2024-12-08 18:16:36 +0530

    Create patter1.java

---
+++ patter1.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n = scan.nextInt();
+        int c=1;
+        for (int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n ; j++){
+            System.out.print(c);
+            c++;
+            }
+        System.out.println();
+        
+       }
+    }   
+}
