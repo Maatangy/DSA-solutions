05a18f591e374835253ae541b717c40b2b297d42
Maatangy <148937742+Maatangy@users.noreply.github.com>
2024-12-12 22:02:46 +0530

    Create pattern5.java

---
+++ pattern5.java
@@ -0,0 +1,29 @@
+*/ * 
+   * * 
+   * * * 
+   * * * * 
+   * * * * *
+   * * * *
+   * * *
+   * *
+   *   /*
+
+  class Solution {
+
+    void printTriangle(int n) {
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print("*"+" ");
+            }
+            
+        System.out.println("");
+        }
+        
+        for(int i=1 ; i<=n-1 ; i++){
+            for(int j=1 ; j<=n-i+1-1 ; j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
