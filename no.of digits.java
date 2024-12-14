f81ce98bcfba8b3f6f36ed350db4322ec69dcb50
Maatangy <148937742+Maatangy@users.noreply.github.com>
2024-12-14 18:45:18 +0530

    Create no.of digits.java

---
+++ no.of digits.java
@@ -0,0 +1,13 @@
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int count =0;
+        while(n>0){
+            count=count+1;
+            n=n/10;
+        }
+        System.out.println("count="+count);
+    }
+}
