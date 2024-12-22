d7be15c9ceeb7659f6148debb89903351b4f4950
Maatangy <148937742+Maatangy@users.noreply.github.com>
2024-12-22 17:35:13 +0530

    Create pattern9.java

---
+++ pattern9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+      Scanner scan= new Scanner(System.in);
+      int n=scan.nextInt();
+      for(int i=1 ; i<=n ; i++){
+          for(int j=1;j<=i ; j++){
+              
+              System.out.print(i+" ");
+          }
+          System.out.println();
+      }
+	}
+}
+*/1
+  2 2 
+  3 3 3 
+  4 4 4 4
+  5 5 5 5 5
+  6 6 6 6 6 6/*
