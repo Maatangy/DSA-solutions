737c37b4d074eb5a52183b34cd96f53d0147d122
Maatangy <148937742+Maatangy@users.noreply.github.com>
2025-01-16 22:11:24 +0530

    Create sum of array elaments.java

---
+++ sum of array elaments.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+        int n=6 , sum=0;
+        int arr[]={3,9,1,4,5,7};
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+        }
+         System.out.println(sum);
+	}
+}
