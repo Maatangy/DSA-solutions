ac6265aafa0fa881a864b09c04a5ed19c219aef5
Maatangy <148937742+Maatangy@users.noreply.github.com>
2025-01-19 11:44:41 +0530

    Create return key element.java

---
+++ return key element.java
@@ -0,0 +1,16 @@
+// Q: index is given array is given return array element present in the given key
+
+public class Main
+{
+	public static void main(String[] args) {
+		int key=2;
+		int [] arr={10, 20, 30, 40, 50};
+		int n=5;
+		for(int i=0;i<n;i++){
+		    if(i==key){
+		        System.out.println(arr[i]);
+		    }
+		}
+		    
+	}	
+}		
