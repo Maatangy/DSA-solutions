82aaac3011b7aeb9a4ff5c9b476a59e031b662aa
Maatangy <148937742+Maatangy@users.noreply.github.com>
2025-01-27 13:55:14 +0530

    Create print 1 to n recursion .java

---
+++ print 1 to n recursion .java
@@ -0,0 +1,11 @@
+//Print numbers from 1 to n without the help of loops. You only need to complete the function printNos() that takes n as a parameter and prints the number from 1 to n recursively.
+class Solution {
+
+    public void printNos(int n) {
+        if(n==0){
+            return ;
+        }
+        printNos(n-1);
+        System.out.print(n+" ");
+    }
+}
